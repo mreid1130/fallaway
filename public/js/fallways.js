ece93edb3ca8a3fc59210c17bcaa9cfa5fa4923f
Mark Reid <mark.francis.reid@gmail.com>
2014-12-28 19:11:57 -0800

    game loop working, ship is moving

--- public/js/fallways.js
+++ public/js/fallways.js
@@ -1,36 +1,63 @@
 function Faller() {
 	this.x = 300;
 	this.y = 300;
+	this.height = 40;
+	this.width = 40;
+	this.speed = 3;
+	this.dir = "right";
 	this.initDisplay();
 }
 
 Faller.prototype = {
-	updateDisplay: function(xcoord, ycoord){
-		console.log(this.x);
-		console.log(this.y);
+
+	updateDisplay: function(){
+		this.$faller.css('top', this.y);
+		this.$faller.css('left', this.x);
 	},
 
 	initDisplay: function(){
 		this.$faller = $("<div class='faller'></div>")
 		$('#gameboard').append(this.$faller);
 		this.$faller.css('position', 'relative');
-		this.$faller.css('top', this.y);
-		this.$faller.css('left', this.x);
 		this.$faller.css('height', 40);
 		this.$faller.css('width', 40);
 		this.$faller.css('background-image', 'url("http://1.bp.blogspot.com/-MQE-zK1mVSE/UdSVGV3GP3I/AAAAAAAAAu8/EOsv__HnS-M/s512/spacestation.png")');
 		this.$faller.css('background-size', '40px 40px');
 		this.$faller.css('background-repeat', 'no-repeat');
+		
+		this.updateDisplay()
+	},
+
+	move: function(){
+		switch(this.dir) {
+			case 'right':
+				this.x += this.speed;
+				break;
+			case 'left':
+				this.x -= this.speed;
+				break;
+			case 'up':
+				this.y += this.speed;
+				break;
+			case 'down':
+				this.y -= this.speed;
+				break;
+		}
+		this.updateDisplay()
 	}
 
 }
 
-Game = {
-	loop: function() {
-		console.log("Game Starting")
-	}
+function Game() {
+	this.gameboard = $('#gameboard');
+	this.faller = new Faller();
+}
+
+Game.prototype.loop = function(){
+	this.faller.move()
 }
 
 $(document).ready(function() {
-	f = new Faller();
+	game = new Game();
+	setInterval(function() { game.loop(); }, 20);
 });
