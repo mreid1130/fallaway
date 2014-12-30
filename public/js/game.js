4e70ee131bb65a4f2796b4bc3bb5a6df728cffd0
Mark Reid <mark.francis.reid@gmail.com>
2014-12-29 19:08:47 -0800

    fixed user firing

--- public/js/game.js
+++ public/js/game.js
@@ -5,10 +5,12 @@ function Game() {
 	this.faller = new Faller(this.$gameboard);
 	this.enemies = [new badGuy(this.$gameboard)];
 	this.shots = []; 
+	this.badShots = [];
 	this.asteroids = [];
 	this.start = Date.now()
 	this.nextBadGuySpawn = this.start + 5000
 	this.nextAsteroidSpawn = this.start + 3000
+	this.enemyFireTime = this.start + 3000
 	this.scrollTollCollection = [];
 }
 
@@ -19,16 +21,31 @@ Game.prototype.loop = function(){
 	this.updateScore();
 	player = this.faller
 	gameshots = this.shots
+	badshots = this.badShots
+
 	if (Date.now() > this.nextBadGuySpawn) {
 		this.enemies.push(new badGuy(this.$gameboard))
 		this.nextBadGuySpawn += 5000
 	}
 
 	if (Date.now() > this.nextAsteroidSpawn){
-		console.log('hello')
 		this.asteroids.push(new Asteroid(this.$gameboard))
 		this.nextAsteroidSpawn += 3000
 	}
+
+	if (Date.now() > this.enemyFireTime) {
+		this.enemies.forEach(function(enemy){
+			badshots.push(enemy.fire())
+		})
+		this.enemyFireTime += 3000
+	}
+
+	badshots.forEach(function(shot){
+		if (player.hit(shot)){
+			player.explode();
+			player.dead = true
+		}
+	})
 
 	this.enemies.forEach(function(enemy){
 		
@@ -51,6 +68,10 @@ Game.prototype.loop = function(){
 	});
 
 	this.shots.forEach(function(shot){
+		shot.move();
+	});
+
+	this.badShots.forEach(function(shot){
 		shot.move();
 	});
 
@@ -81,12 +102,15 @@ Game.prototype.loop = function(){
 	this.shots = _(this.shots).reject(function(shot){
 		return shot.outOfBounds
 	});
+	this.badShots = _(this.badShots).reject(function(shot){
+		return shot.outOfBounds
+	});
 	this.asteroids = _(this.asteroids).reject(function(asteroid){
 		return asteroid.outOfBounds
 	});
 }
 
-Game.prototype.fire = function(){
+Game.prototype.userFire = function(){
 	this.shots.push(new UserOrb(this.$gameboard, this.faller))
 }
 
@@ -102,7 +126,7 @@ $(document).ready(function() {
 			game.loop(); 
 		} else {
 			window.confirm("Game over, restarting...");
-			location.reload();
+			location.reload(true);
 		}
 	}, 20);
 
@@ -114,7 +138,7 @@ $(document).ready(function() {
 	});
 
 	Mousetrap.bind('space', function(){
-		game.fire();
+		game.userFire();
 	})
 });
 
