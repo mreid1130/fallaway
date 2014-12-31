e0273faa890527c5f2d4f615683d4f2154a7dfc0
Mark Reid <mark.francis.reid@gmail.com>
2014-12-30 23:18:24 -0800

    implemented user auth, player validations, gravatar

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,28 @@
 class Player < ActiveRecord::Base
-  # Remember to create a migration!
+	has_many :games
+	validates :email, :username, {presence: true, uniqueness: true}
+	validates :password_hash, presence: true
+	validates :email, format: {with: /\S+@{1}\S+[.]\D{2,}/, message: 'is not a valid email address'}
+	
+	before_save :gravatar_src
+
+	def gravatar_src
+		email_address = self.email.downcase
+		hash = Digest::MD5.hexdigest(email_address)
+		self.gravatar = "http://www.gravatar.com/avatar/#{hash}"
+	end
+
+	def password
+	  @password ||= BCrypt::Password.new(password_hash) if password_hash
+	end
+
+	def password=(new_password)
+	  @password = BCrypt::Password.create(new_password)
+	  self.password_hash = @password
+	end
+
+	def authenticate(password)
+	  self.password_hash == password
+	end
+
 end
